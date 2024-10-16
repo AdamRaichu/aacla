e9adc617897ffb9d27142bf06f65faa8253bfd2b
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2024-10-15 21:04:59 -0400

    now it works more than once

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -14,27 +14,28 @@ export default async function PluginMain() {
 
     const doc = context.m_popup.document;
 
-    console.log("before findElement");
-    // Wait for the popup to exist.
-    await Millennium.findElement(doc, formSelector);
-    console.log("after findElement");
+    while (true) {
+      // Wait for the popup to exist.
+      await Millennium.findElement(doc, formSelector);
 
-    const _header = await Millennium.findElement(doc, `${formSelector} .DialogHeader`); // form.querySelector("div.DialogHeader") as HTMLDivElement;
-    const header = _header[0] as HTMLDivElement;
-    console.log(header.innerText);
-    if (containsAllOf(header.innerText, headerKeywords)) {
-      const _confirmButton = await Millennium.findElement(doc, `${formSelector} button[type='submit']`);
-      const confirmButton = _confirmButton[0] as HTMLButtonElement;
-      if (confirmButton.innerText === "Continue") {
-        console.log("Skipped popup.");
-        confirmButton.click();
+      const _header = await Millennium.findElement(doc, `${formSelector} .DialogHeader`); // form.querySelector("div.DialogHeader") as HTMLDivElement;
+      const header = _header[0] as HTMLDivElement;
+      if (containsAllOf(header.innerText, headerKeywords)) {
+        const _confirmButton = await Millennium.findElement(doc, `${formSelector} button[type='submit']`);
+        const confirmButton = _confirmButton[0] as HTMLButtonElement;
+        if (confirmButton.innerText === "Continue") {
+          console.log("%cSkipping confirmation popup.", "color: orange; font-weight: bold;");
+          confirmButton.click();
+        } else {
+          console.warn("Button text was not 'Continue '.");
+          console.warn(`"${confirmButton?.innerText}"`);
+        }
       } else {
-        console.warn("Button text was not 'Continue '.");
-        console.warn(`"${confirmButton?.innerText}"`);
+        console.warn("Popup was not for launching game with custom args.");
+        console.warn(`"${header.innerText}"`);
       }
-    } else {
-      console.warn("Popup was not for launching game with custom args.");
-      console.warn(`"${header.innerText}"`);
+      // Surely you can't get the popup more than once every 3 seconds, right?
+      await new Promise((resolve) => setTimeout(resolve, 3000));
     }
   });
 }
